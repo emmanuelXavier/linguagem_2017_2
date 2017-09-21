f7f64e326293ac1bcd9ae83111aa34c5b081a46f
Emmanuel Xavier <emanuelsx@hotmail.com>
2017-09-20 22:01:30 -0300

    atualiza o repositório

--- source-04/main.c
+++ source-04/main.c
@@ -4,15 +4,15 @@ int main(int argc, char **argv)
 {
 	int n[10];
 	int i;
-	//for (i = 0; i < 10; i++)
-		//printf("%d\n",&n[i]);
+	for (i = 0; i < 10; i++)
+		printf("%d\n",&n[i]);
 		
 	/*for (i = 0; i < 10;i++)
 		scanf("%d",&n[i]);*/
 	
 	printf("\n\nListando\n\n");	
 	for (i = 2; i < 50;i += 2)
-			printf("%d. Emmanuel é belo\n",i);
+			printf("%d. Emmanuel é bonito\n",i);
 
 
 
